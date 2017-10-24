b8cb29422226f3f4ad6b20699a95326ee50c1b8a
Maria Mata <mariajo.mata@gmail.com>
2017-10-24 10:41:11 -0600

    center the svg art on the page

--- src/components/Artboard.vue
+++ src/components/Artboard.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section">
     <div class="container">
-      <div id="drawing">
+      <div id="drawing" class="content has-text-centered">
         <!-- SVG art goes here -->
       </div>
     </div>
