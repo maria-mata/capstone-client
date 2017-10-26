bab5520093c8206180c9d6950cf29413a976c397
Maria Mata <mariajo.mata@gmail.com>
2017-10-26 12:27:32 -0600

    add signin POST code

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -3,11 +3,11 @@
     <div class="container">
       <div class="box">
         <h2 class="title">Sign In</h2>
-        <form>
+        <form @submit.prevent="signIn">
           <div class="field">
             <label class="label">Username</label>
             <div class="control">
-              <input class="input" type="text" placeholder="Username">
+              <input v-model="signInForm.username" class="input" type="text" placeholder="Username">
             </div>
             <!-- <p class="help is-success">This username is available</p> -->
             <!-- <p class="help is-danger">This email is invalid</p> -->
@@ -15,7 +15,7 @@
           <div class="field">
             <label class="label">Password</label>
             <div class="control">
-              <input class="input" type="password" placeholder="Password">
+              <input v-model="signInForm.password" class="input" type="password" placeholder="Password">
             </div>
             <!-- <p class="help is-success">This username is available</p> -->
             <!-- <p class="help is-danger">This email is invalid</p> -->
@@ -25,7 +25,7 @@
               <button class="button is-primary">Submit</button>
             </div>
             <div class="control">
-              <button class="button is-link">Cancel</button>
+              <button type="submit" class="button is-link">Cancel</button>
             </div>
           </div>
         </form>
@@ -35,7 +35,36 @@
 </template>
 
 <script>
+const url = 'https://moodpix.herokuapp.com'
+// const url = 'http://localhost:5000'
+
 export default {
-  name: 'signin'
+  name: 'signin',
+  data() {
+    return {
+      signInForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    signIn() {
+      const settings = {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: this.signInForm.username,
+          password: this.signInForm.password
+        })
+      }
+      fetch(`${url}/signin`, settings)
+      .then(response => response.json())
+      .then(response => console.log(response))
+    }
+  }
 }
 </script>
